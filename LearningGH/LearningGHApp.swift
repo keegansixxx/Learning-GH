98997d3844f962c6cd3df883dbea4720ccd90858
Keegan Pangowish <keegan>
2021-12-05 16:28:42 -0800

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Keegan Pangowish on 2021-12-05.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
